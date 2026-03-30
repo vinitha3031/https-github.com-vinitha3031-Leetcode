301843ac6c0ccbf1dadc66020fadd1d1f324d6d7
vinitha3031 <vinithagopi31@gmail.com>
2026-03-30 13:32:41 +0530

    Time: 0 ms (100%), Space: 19.2 MB (73.74%) - LeetHub

---
+++ 2839-check-if-strings-can-be-made-equal-with-operations-i/2839-check-if-strings-can-be-made-equal-with-operations-i.py
@@ -0,0 +1,16 @@
+class Solution:
+    def canBeEqual(self, s1: str, s2: str) -> bool:
+        a=""
+        b=""
+        c=""
+        d=""
+        for i in range(len(s1)):
+            if i%2!=0:
+                a+=s1[i]
+                c+=s2[i]
+            else:
+                b+=s1[i]
+                d+=s2[i]
+        return sorted(a)==sorted(c) and sorted(b)==sorted(d)
+        
+        
